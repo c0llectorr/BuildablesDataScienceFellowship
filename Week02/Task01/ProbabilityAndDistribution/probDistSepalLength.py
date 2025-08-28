541ba21bbdc5bac1ea75083ea1993eb25f8d959e
Muhammad Ahmad <116812294+c0llectorr@users.noreply.github.com>
2025-08-28 21:52:31 +0000

    Week 02 Task 01 Late Submission Due to Urgent Piece of Work at Home

--- Week02/Task01/ProbabilityAndDistribution/probDistSepalLength.py
+++ Week02/Task01/ProbabilityAndDistribution/probDistSepalLength.py
@@ -3,7 +3,5 @@ from sklearn.datasets import load_iris
 iris = load_iris(as_frame=True)
 df = iris.frame
 
-print(df.var())
-
-
-
+sepal_length_probs = df['sepal length (cm)'].value_counts(normalize=True).sort_index()
+print(sepal_length_probs)
