3aea62938556c52c43aec58cf1f3c5cf4c6066c7
Muhammad Ahmad <116812294+c0llectorr@users.noreply.github.com>
2026-01-21 19:42:36 +0500

    Delete Week01 directory

--- Week01/Task02/squareList.py
+++
@@ -1,8 +0,0 @@
-def createSquareList():
-    return [i * i for i in range(1, 11)]
-
-def main():
-    print(createSquareList())
-    
-if __name__ == "__main__":
-    main()
--- Week01/Task04/6x6Matrics.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def main():
-    data = np.random.randint(1,100, size=(6,6))
-    print(f"Original 6x6 Matrix:\n{data}")
-    
-if __name__ == "__main__":
-    main()
